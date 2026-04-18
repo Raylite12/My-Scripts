469ff0796458c5a7ec9a5382e8872c48d677e374
Raylite12 <donnell2828@yahoo.com>
2026-04-18 19:06:25 -0400

    Add files via upload

--- password.ps1
+++ password.ps1
@@ -13,28 +13,3 @@ param([int]$length = 20)
 	-join (Get-Random -Count $length -InputObject $StringSet)
 }
 New-RandomPassword
-
-#Ask for input
-param([Parameter(Mandatory)][string]$fullName)
-
-$action = New-ScheduledTaskAction -Execute "Taskmgr.exe"
-$trigger = New-ScheduledTaskTrigger -AtLogon
-$principal = "Contoso\Administrator"
-$settings = New-ScheduledTaskSettingsSet
-$task = New-ScheduledTask -Action $action -Principal $principal -Trigger $trigger -Settings $settings
-Register-ScheduledTask T1 -InputObject $task
-
-<# In this example, the set of commands uses several cmdlets and variables to define and then register a scheduled task.
-
-The first command uses the New-ScheduledTaskAction cmdlet to assign the executable file tskmgr.exe to the variable $action.
-
-The second command uses the New-ScheduledTaskTrigger cmdlet to assign the value AtLogon to the variable $trigger.
-
-The third command assigns the principal of the scheduled task Contoso\Administrator to the variable $principal.
-
-The fourth command uses the New-ScheduledTaskSettingsSet cmdlet to assign a task settings object to the variable $settings.
-
-The fifth command creates a new task and assigns the task definition to the variable $task.
-
-The sixth command (hypothetically) runs at a later time. It registers the new scheduled task and defines it by 
-using the $task variable. #>
