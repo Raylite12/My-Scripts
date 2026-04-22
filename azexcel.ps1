c7c6d72326f0f85c2e07036b9995bce321e024ef
Raylite12 <donnell2828@yahoo.com>
2026-04-22 08:04:12 -0400

    Add files via upload

---
+++ azexcel.ps1
@@ -0,0 +1,105 @@
+[net.servicePointManager]::SecurityProtocol = 
+[Net.ServicePointManager]::SecurityProtocol -Bor 
+[Net.SecurityProtocolType]::Tls12
+
+Connect-AzAccount -Tenant
+Connect-MgGraph
+Get-MgContext | fl *
+Import-Module Microsoft.Graph.Users
+Import-Module Microsoft.Graph.Groups
+
+Get-MgGroup | Format-List DisplayName | Out-File -FilePath c: -Append
+Get-MgUser | Format-List DisplayName | Out-File -FilePath c: -Append
+
+function commandlr {
+    #Define the path to your input file 
+    $inputFile = "\\corp.nsa.gov\NCMD\Home\vol32031\ldraye\Private\Larry\command1.txt"
+
+    #Define the path for the output Excel file
+    $outputTxt = "\\corp.nsa.gov\NCMD\Home\vol32031\ldraye\Private\Larry1"
+
+    #Get the content of the text file
+    $textContext = Get-Content $inputFile 
+
+    #Create an Excel Com Object
+    $excel = New-Object -ComObject Excel.Application
+    $excel.Visible = $true       #Make Excel visible (remove or set false for background operation)
+
+    #Create a new workbook
+    $workbook = $excel.workbooks.Add()
+
+    #Get the first worksheet 
+    #Access the first default sheet and rename it
+    $sheetGroup = $workbook.Sheets.Item(1)   #Access the first sheet by index 
+    $sheetGroup.Name = "Command"
+
+    #Add a title to the worksheet
+    $sheetGroup.Cells.Item(1, 1).value = "Command Type"
+    $sheetGroup.Cells.Item(1, 1).Font.Bold = $true
+    $sheetGroup.Cells.Item(1, 1).Font.Size = 12
+    $sheetGroup.Cells.Item(1, 1).Interior.Color = [System.Drawing.ColorTranslator]::ToOle([System.Drawing.Color]::YellowGreen)
+
+    $sheetGroup.Cells.Item(1, 2).value = "Name"
+    $sheetGroup.Cells.Item(1, 2).Font.Bold = $true
+    $sheetGroup.Cells.Item(1, 2).Font.Size = 12
+    $sheetGroup.Cells.Item(1, 2).Interior.Color = [System.Drawing.ColorTranslator]::ToOle([System.Drawing.Color]::BurlyWood)
+
+    #Add a second sheet and rename it 
+    $sheetUser = $workbook.Sheets.add()
+    $sheetUser.Name = "User"
+
+    $sheetUser.Cells.Item(1, 1).value = "Command Type"
+    $sheetUser.Cells.Item(1, 1).Font.Bold = $true
+    $sheetUser.Cells.Item(1, 1).Font.Size = 12
+    $sheetUser.Cells.Item(1, 1).Interior.Color = [System.Drawing.ColorTranslator]::ToOle([System.Drawing.Color]::YellowGreen)
+
+    $sheetUser.Cells.Item(1, 2).value = "Name"
+    $sheetUser.Cells.Item(1, 2).Font.Bold = $true
+    $sheetUser.Cells.Item(1, 2).Font.Size = 12
+    $sheetUser.Cells.Item(1, 2).Interior.Color = [System.Drawing.ColorTranslator]::ToOle([System.Drawing.Color]::BurlyWood)
+
+    #Function to set the order of sheets
+    function Set-SheetOrder {
+        param (
+            $workbook,
+            [string[]]$order
+        )
+        $sheetCount = $workbook.Sheets.$sheet.Count
+        foreach ($name in $order) {
+            $sheet = $workbook.Sheets.Item($name)
+            $sheet.Move($null, $sheetCount)
+            $sheetCount--
+        }
+        
+    }
+    #Set the order of the sheets
+    Set-SheetOrder -workbook $workbook -order@("Group", "Users")
+
+   #Populate the Excel sheet with data from the text file    
+for ($i = 0; $i -lt $textContext.Length; $i++) {
+    $sheet.cells.Item($i + 2, 1).Value = $textContext[$i]
+
+    $columns = $textContext[$i].Split(' ', [StringSplitOptions]::RemoveEmptyEntries)
+    for ($j = 0; $j -lt $columns.Length; $j++) {
+        $sheet.cells.item($i + 2, $j + 1).Value = $columns[$j]
+    }
+}
+
+#Autofit and Font Name the columns
+$sheet.UsedRange.Font.Name = "Times New Roman"
+$sheet.UsedRange.Font.Size = 12
+$sheet.UsedRange.Cells.EntireColumn.AutoFit()
+
+#Save the workbook
+$workbook.SaveAs($outputTxt)
+
+#Clean up
+$Excel.Quit()
+[System.Runtime.InteropServices.Marshal]::ReleaseComObject($sheet) | Out-Null
+[System.Runtime.InteropServices.Marshal]::ReleaseComObject($workbook) | Out-Null
+[System.Runtime.InteropServices.Marshal]::ReleaseComObject($excel) | Out-Null
+[GC]::Collect()
+[GC]::WaitForPendingFinalizers()
+
+}
+commandlr
